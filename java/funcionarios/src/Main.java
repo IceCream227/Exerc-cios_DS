983e861f52cf7cfe63f7d022bfbd689513ceadcd
Ice227 <87146621+IceCream227@users.noreply.github.com>
2026-06-11 10:23:22 -0300

    Update Main.java

--- java/funcionarios/src/Main.java
+++ java/funcionarios/src/Main.java
@@ -1,13 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
-       Matricula p1 = new Matricula(123456, "Alberto", "Engenheiro", 3500);
-       Matricula p2 = new Matricula(0,"Luana", "Designer", 1600);
-       Matricula p3 = new Matricula(123455, "Augusto", "Médico", 5000);
+        Scanner entrada = new Scanner(System.in);
 
-       p1.Show();
-       System.out.println();
-       p2.Show();
-       System.out.println();
-       p3.Show();
+        List<Matricula> listaFuncionario = new ArrayList<>();
+
+        for (int i = 1; i<=2; i++){
+            System.out.println("Funcionário "+i);
+            System.out.println("Matrícula: ");
+            int matricula = entrada.nextInt();
+            entrada.nextLine();
+
+            System.out.println("Nome: ");
+            String nome = entrada.nextLine();
+
+            System.out.println("Cargo: ");
+            String cargo = entrada.nextLine();
+
+            System.out.println("Salário: ");
+            int salario = entrada.nextInt();
+
+            Matricula funcionario = new Matricula(matricula, nome, cargo, salario);
+            listaFuncionario.add(funcionario);
+
+        }
+        System.out.println("\nLista de Funcionários");
+        for (Matricula f:listaFuncionario){
+            f.Show();
+            System.out.println();
+        }
+        entrada.close();
     }
 }
